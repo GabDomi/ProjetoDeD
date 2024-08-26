e199953864b37a78a995ed3b80352208e761bcc6
Gabriele Dominico <121459497+GabDomi@users.noreply.github.com>
2024-08-26 16:45:34 -0300

    Update TestPersonagem.kt

--- ProjetoDeD/src/test/kotlin/TestPersonagem.kt
+++ ProjetoDeD/src/test/kotlin/TestPersonagem.kt
@@ -6,16 +6,12 @@ class PersonagemTest {
 
     @Test
     fun testPontosDeVida() {
-        // Criar uma instância do personagem
         val personagem = Personagem()
 
-        // Definir o valor de constituição
         personagem.constituicao = 14
 
-        // Calcular os pontos de vida esperados
         val pontosDeVidaEsperados = 10 + (personagem.constituicao - 10) / 2
 
-        // Verificar se o cálculo dos pontos de vida está correto
         assertEquals(pontosDeVidaEsperados, personagem.pontosDeVida)
     }
 }
